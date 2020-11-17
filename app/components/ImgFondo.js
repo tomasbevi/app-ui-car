9f51d7085fca15055e694d634abc6b1ae450257d
Tomas Bevilacqua <tomasbevilacqua@MacBook-Pro-de-Tomas.local>
2020-11-16 22:24:15 -0300

    delete item que no se usa

--- app/components/ImgFondo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Image } from 'react-native'
-
-const ImagesExample = () => (
-   <Image source = {require('../assets/background.jpg')}
-   />
-)
-export default ImagesExample
